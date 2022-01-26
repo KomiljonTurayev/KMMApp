82c3dcc02a3609f8e7feaca8d8b807cc6cade95f
turay <turayevkomiljon@gmail.com>
2022-01-26 16:43:06 +0500

    Initial commit

---
+++ settings.gradle.kts
@@ -0,0 +1,11 @@
+pluginManagement {
+    repositories {
+        google()
+        gradlePluginPortal()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "KMMApp"
+include(":KMMApp")
+include(":shared")
